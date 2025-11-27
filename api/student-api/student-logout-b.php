364164753335754cb135cc69ea1ba005cc468f7a
bdumdum <bdummy0531@gmail.com>
2025-11-27 15:47:29 +0800

    mdoified logout

--- api/student-api/student-logout-b.php
+++ api/student-api/student-logout-b.php
@@ -1,7 +1,7 @@
 <?php
-require_once __DIR__ . "/../../db/config.php";
+require_once "../../db/config.php";
 
 session_destroy();
-header("Location: /eduqueue-main/index.php");
+header("Location: ../../index.php");
 exit;
 ?>
